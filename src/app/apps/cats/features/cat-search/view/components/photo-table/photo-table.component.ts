380206fca70f3d33dd0dc4365322936782095a88
yuri-rudenko <yurii.rudenko26@gmail.com>
2025-04-21 09:32:44 +0000

    added styles

--- src/app/apps/cats/features/cat-search/view/components/photo-table/photo-table.component.ts
+++ src/app/apps/cats/features/cat-search/view/components/photo-table/photo-table.component.ts
@@ -2,26 +2,27 @@ import {Component, effect, inject, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {getCats} from '../../../data-access/state/cat.actions';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {CommonModule} from '@angular/common';
+import {AsyncPipe, CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-photo-table',
-  imports: [CommonModule],
+  imports: [AsyncPipe],
   templateUrl: './photo-table.component.html',
   styleUrl: './photo-table.component.css',
   standalone: true,
 })
 export class PhotoTableComponent implements OnInit {
   private store = inject(Store<{cats: any[]}>);
-  cats = toSignal(this.store.select('cats'), { initialValue: [] });
+  cats = this.store.select('cats');
 
   constructor() {
     effect(() => {
-      console.log('Cats updated:', this.cats());
+      console.log('Cats updated:', this.cats);
     });
   }
 
   ngOnInit(): void {
     this.store.dispatch(getCats({}));
   }
+
 }
